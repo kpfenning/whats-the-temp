05f11aa59feaceb16e5cbfe5f9e006f91863aff9
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-07 15:20:07 -0800

    rethinking html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 var apiKey = "6266ba4a3f32a524499faffac708e3b9";
 
 
+var currentWeather = function (city) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}`)
+    .then(function (response) {
+        return response.json();
+     })
+     .then(function (response) {
+        var cityLon = response.city.coord.lon;
+        var cityLat = response.city.coord.lat;
 
+        
+         
