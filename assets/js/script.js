9b08cfa61e78204d8bcad5326dc985afb1beaa69
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-09 10:37:03 -0800

    matched up id's & classes for variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
 var apiKey = "6266ba4a3f32a524499faffac708e3b9";
 
-var searchHistory = $("#history-list");
-var searchCity = $("#city-search");
-var searchBtn = $("#searchBtn");
-var clearHistory = $("#clear-history");
+var currentCity = document.querySelector('#city-name');
+var searchBtn = document.querySelector('#search-btn');
+var searchHistory = document.getElementById('search-history');
+var currentForecast = document.querySelector('.current-forecast');
+var forecastContainer = document.getElementById('weather');
+var weeklyForecast = document.querySelector('.weekly-forecast');
 
-var currentCity = $("#current-city");
-var currentTemp = $("#current-temp");
-var currentHumidity = $("#current-humid");
-var currentWind = $("#wind-speed");
-var currentUV = $("#uv-index");
+var recentSearches = [];
 
-var weatherContent = $("#forecast");
-var cities = [];
 
-var currentDate = moment().format("L");
-$("#current-date").text(currentDate);         
+        
